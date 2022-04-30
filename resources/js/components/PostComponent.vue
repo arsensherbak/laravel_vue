1bf077d02ca7c1095885e4c794fa17a8f56dabf0
arsensherbak <sherbakars@gmail.com>
2022-05-01 02:02:54 +0300

    laravel and vue

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <CreateComponent></CreateComponent>
+        <IndexComponent></IndexComponent>
+    </div>
+</template>
+
+<script>
+import CommentComponent from "./CommentComponent";
+import CreateComponent from "./CreateComponent";
+import IndexComponent from "./IndexComponent";
+
+export default {
+    name: "PostComponent",
+    components: {
+        CommentComponent,
+        CreateComponent,
+        IndexComponent
+    },
+    data() {
+        return {}
+    },
+
+    computed: {},
+
+}
+</script>
+
+<style scoped>
+
+</style>
